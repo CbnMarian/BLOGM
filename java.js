07a1dbb39c1e656ef752e67d67b07c04d32b2509
Ciobanu Marian <m.marian03@yahoo.com>
2023-03-19 20:41:38 +0200

    Version 1

---
+++ java.js
@@ -0,0 +1,14 @@
+/* (function () {
+  var a=document.createElement("p");
+  continut=document.createTextNode("TOP TEN BEST SOUNDING MOTORCYCLES");
+  informatii=document.createTextNode("There are many reasons as to why you should buy a motorcycle, but the sound these 10 bikes make is enough to make us immediately want to buy them.")
+  
+
+  a.appendChild(continut);
+  document.body.appendChild(a);
+  a.appendChild(informatii);
+  document.body.appendChild(a);
+  
+}
+());
+ */
